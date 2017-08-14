1235e017aae658cda35eb118cd1b1c0da39b74d8
ajimal1992 <ajimal@hotmail.com>
2017-08-14 16:18:40 +0800

    Changes to generated file

--- lib/boilerplate/Template/TemplateBoilerplate.h
+++ lib/boilerplate/Template/TemplateBoilerplate.h
@@ -61,11 +61,14 @@ namespace TemplateMsg
     {
         public:
             Subscriber(DDSBoilerplate * boiler_object, char const * user_topic);
+            Subscriber(DDSBoilerplate * boiler_object, char const * user_topic, char const * attribute); //ADDED
             int init_subscriber();
+            int init_subscriber(char const * attribute); //ADDED
             int kill();
             TemplateSeq get_data_seq();
             DDS_SampleInfoSeq get_info_seq();
             bool is_read();
+            void set_string_filter(char* filter); //ADDED
 
         private:
             char const * user_topic_;
@@ -73,6 +76,7 @@ namespace TemplateMsg
             DDSDomainParticipant *participant_;
             TemplateListener *reader_listener_;
             DDS_ReturnCode_t retcode_;
+            DDSContentFilteredTopic *cft_;
     };
 }
 #endif
